68d0579ef6ed358571f69d386fe5a1a974617d6b
Mikhael Vinicius <mikhaelsoel5207418@gmail.com>
2026-07-16 17:22:23 -0300

    Primeiro commit

---
+++ src/main/java/com/example/DocuMind/DocuMindApplication.java
@@ -0,0 +1,13 @@
+package com.example.DocuMind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocuMindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocuMindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DocuMind/DocuMindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DocuMind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocuMindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
